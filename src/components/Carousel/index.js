e295b03c46256fe2f823f4bc4a3ff055ef6c1d2f
Michał Wieczorek <m98.wieczorek@onet.pl>
2022-08-25 23:31:21 +0200

    Carousel simplified

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -42,48 +42,32 @@ class Carousel extends React.Component {
           }
         : { height: '100%' }
 
-    if (gallery.length === 1) {
-      return (
-        <div className={`carousel ${className}`} style={style} {...props}>
-          <img
-            src={gallery[0]}
-            alt={alt}
-            className='carousel__image'
-            style={imgStyles}
-          />
-        </div>
-      )
-    }
-
     return (
-      <div
-        className={`carousel carousel ${className}`}
-        style={style}
-        {...props}
-      >
+      <div className={`carousel ${className}`} style={style} {...props}>
         <img
           src={gallery[this.state.currentImageIndex]}
           alt={alt}
-          className={`carousel__image`}
+          className='carousel__image'
           style={imgStyles}
         />
-
-        <div className='carousel__actions'>
-          <Image
-            role='button'
-            src={CaretLeftSvg}
-            alt='Get previous image'
-            className='carousel__btn'
-            onClick={() => this.changeImage(this.state.currentImageIndex - 1)}
-          />
-          <Image
-            role='button'
-            src={CaretRightSvg}
-            alt='Get next image'
-            className='carousel__btn'
-            onClick={() => this.changeImage(this.state.currentImageIndex + 1)}
-          />
-        </div>
+        {gallery.length > 1 && (
+          <div className='carousel__actions'>
+            <Image
+              role='button'
+              src={CaretLeftSvg}
+              alt='Get previous image'
+              className='carousel__btn'
+              onClick={() => this.changeImage(this.state.currentImageIndex - 1)}
+            />
+            <Image
+              role='button'
+              src={CaretRightSvg}
+              alt='Get next image'
+              className='carousel__btn'
+              onClick={() => this.changeImage(this.state.currentImageIndex + 1)}
+            />
+          </div>
+        )}
       </div>
     )
   }
